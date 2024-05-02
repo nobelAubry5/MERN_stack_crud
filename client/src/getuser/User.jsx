8053d0a1f5270826860889e7eb663909cc4663c3
Aubry Nobel Niyonkuru <nobelaubry5@gmail.com>
2024-05-02 13:35:24 -0400

    Axios-GET-Terminé

--- client/src/getuser/User.jsx
+++ client/src/getuser/User.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BsFillTrash3Fill } from "react-icons/bs";
 import { FaPenToSquare } from "react-icons/fa6";
 import { FaUserPlus } from "react-icons/fa6";
 import "./user.css";
+import axios from "axios";
+
 const User = () => {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:8000/api/users");
+        setUsers(response.data);
+      } catch (error) {
+        console.log("Error while fetching data", error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="userTable">
       <button type="button" class="btn btn-primary">
@@ -20,20 +35,24 @@ const User = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>John</td>
-            <td>john@gmail.com</td>
-            <td>Québec, Canada</td>
-            <td className="actionsButtons">
-              <button type="button" class="btn btn-warning">
-                <FaPenToSquare />
-              </button>{" "}
-              <button type="button" class="btn btn-danger">
-                <BsFillTrash3Fill />
-              </button>
-            </td>
-          </tr>
+          {users.map((user, index) => {
+            return (
+              <tr>
+                <td>{index + 1}</td>
+                <td>{user.name}</td>
+                <td>{user.email} </td>
+                <td>{user.address}</td>
+                <td className="actionsButtons">
+                  <button type="button" class="btn btn-warning">
+                    <FaPenToSquare />
+                  </button>{" "}
+                  <button type="button" class="btn btn-danger">
+                    <BsFillTrash3Fill />
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
--- server/index.js
+++ server/index.js
@@ -3,14 +3,15 @@ import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import route from "./routes/userRoute.js";
+import cors from "cors";
 
 const app = express();
 
 app.use(bodyParser.json());
-
+app.use(cors());
 dotenv.config();
 
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 8000;
 const MONGOURL = process.env.MONGO_URL;
 
 mongoose
