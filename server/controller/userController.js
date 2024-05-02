21be02b1511fb68121f776666f35020f149d8fdc
Aubry Nobel Niyonkuru <nobelaubry5@gmail.com>
2024-05-02 11:10:22 -0400

    Avoir chaque utilisateur - Terminé

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -11,7 +11,7 @@ export const create = async (req, res) => {
     }
     const savedData = await newUser.save();
     //res.status(200).json(savedData);
-    res.status(200).json({ message: "User created successfully." });
+    res.status(200).json({ message: "Création de l'utilisateur réussi" });
   } catch (error) {
     res.status(500).json({ errorMessage: error.message });
   }
@@ -26,6 +26,21 @@ export const getAllUsers = async (req, res) => {
         .json({ message: "Données d'utilisateurs indisponible" });
     }
     res.status(200).json(userData);
+  } catch (error) {
+    res.status(500).json({ errorMessage: error.message });
+  }
+};
+
+export const getUserById = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const userExist = await User.findById(id);
+    if (!userExist) {
+      return res
+        .status(404)
+        .json({ message: "Données d'utilisateurs indisponible" });
+    }
+    res.status(200).json(userExist);
   } catch (error) {
     res.status(500).json({ errorMessage: error.message });
   }
